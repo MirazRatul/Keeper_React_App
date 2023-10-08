2b57ab0d8b4cdf2eaa02a4f0a88fe7c87c1dbc5d
MirazRatul <90981967+MirazRatul@users.noreply.github.com>
2023-10-09 04:05:12 +0600

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const customStyle = {
+  background: "#f5ba13",
+  color: "white"
+};
+
+function Header() {
+  return <h1 style={customStyle}>Keeper</h1>;
+}
+
+export default Header;
